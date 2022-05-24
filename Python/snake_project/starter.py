f436a6ecd223ebe1e0bf13a5bd485116ad36cc6e
Jm <spawn2x@protonmail.com>
2022-05-24 15:11:57 +0200

    update snake

--- Python/snake_project/starter.py
+++ Python/snake_project/starter.py
@@ -1,4 +1,5 @@
 # Import essential modules - 1
+from ctypes import pointer
 from urllib.request import CacheFTPHandler
 import pygame
 import sys
@@ -10,44 +11,56 @@ import random
 class Snake:
     # Constructor - 3
     def __init__(self):
-        self.lengh = 1
-        self.positions = [((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))]
+        # Fill the init method with snake attributes : lenght, initial position, initial direction and color - 27
+        self.length = 1
+        self.position = [((SCREEN_WIDTH_IN_PIXEL / 2), (SCREEN_HEIGHT_IN_PIXEL / 2))]
         self.direction = random.choice([UP, DOWN, LEFT, RIGHT])
         self.color = (17, 24, 47)
-        # Fill the init method with snake attributes : lenght, initial position, initial direction and color - 27
+        self.speed = 10
+        self.score = 0
 
     # Method to get the position of the head of the snake - 4
     def get_head_position(self):
         # Return the position of the head of the snake - 28
-        return self.positions [0]
+        return self.position[0]
 
     # Method to turn - 5
-    def turn(self):
-        pass
+    def turn(self, point):
         # If the snake is more than 1 case long, it can't go in the direction it is coming - 29
-
+        if self.length > 1 and point == (self.direction[0]*(-1), self.direction[1]*(-1)):
+            return
         # Otherwise, it changes its position - 30
-
+        else:
+            self.direction = point
+            print(f"Self dir = {point}")
     # Method to move - 6
     def move(self):
-        pass
         # Calculate the new position of the snake given the current one and the direction - 31
-
+        current_head_position = self.get_head_position()
+        new_head_position = ((current_head_position[0] + self.direction[0]*CASE_SIZE_IN_PIXEL)%SCREEN_WIDTH_IN_PIXEL, (current_head_position[1] + self.direction[1]*CASE_SIZE_IN_PIXEL)%SCREEN_WIDTH_IN_PIXEL)
         # If the head of the snake collides with its body, reset the game - 32
-
-        # Otherwise, add the new position at the beginning of the list of positions and remove the last one - 33
-
+        if self.length > 4 and new_head_position in self.position[4:]:
+            self.reset()
+        # Otherwise, add the new position at the beginning of the list of position and remove the last one - 33
+        else:
+            self.position.insert(0, new_head_position)
+            if len(self.position) > self.length:
+                self.position.pop()
     # Method to reset the snake in case of colision - 7
     def reset(self):
-        pass
         # Reset the snake with initial attributes - 34
+        self.length = 1
+        self.position = [((SCREEN_WIDTH_IN_PIXEL / 2), (SCREEN_HEIGHT_IN_PIXEL / 2))]
+        self.direction = random.choice([UP, DOWN, LEFT, RIGHT])
+        self.speed = 10
+        self.score = 0
 
     # Method to draw/display the snake on the board - 8
     def draw(self, surface):        
         # For each part of the snake, create and draw a rectangle of a different color than the board - 35
-        for part in self.positions:
-            snake = pygame.Rect((part[0], part[1]), (CASE_SIZE, CASE_SIZE))
-            pygame.draw.rect(surface, (0, 0, 0), snake)
+        for part in self.position:
+            snake = pygame.Rect((part[0], part[1]), (CASE_SIZE_IN_PIXEL, CASE_SIZE_IN_PIXEL))
+            pygame.draw.rect(surface, self.color, snake)
 
 
     # Method to handle (manipuler, traiter) the keys inputs - 9
@@ -59,46 +72,55 @@ class Snake:
                 pygame.quit()
                 sys.exit()
             # Otherwise, depending on the pressed key, turn the snake in the corresponding position - 38
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_z:
+                    self.turn(UP)
+                elif event.key == pygame.K_s:
+                    self.turn(DOWN)
+                elif event.key == pygame.K_q:
+                    self.turn(LEFT)
+                elif event.key == pygame.K_d:
+                    self.turn(RIGHT)
 
 
 # Create the food class - 2 bis
 class Food:
     # Contructor - 10
     def __init__(self):
-        pass
         # Fill the init method with food attributes : initial position and color - 39
-
+        self.position = ((random.randint(0, NB_OF_CASE_WIDTH-1) * CASE_SIZE_IN_PIXEL), (random.randint(0, NB_OF_CASE_HEIGHT-1) * CASE_SIZE_IN_PIXEL))
+        self.color = (233, 163, 49)
     # Method to randomize a position for the food - 11
     def randomize_food(self):
-        pass
         # Randomize the position - 40
-
+        self.position = ((random.randint(0, NB_OF_CASE_WIDTH-1) * CASE_SIZE_IN_PIXEL), (random.randint(0, NB_OF_CASE_HEIGHT-1) * CASE_SIZE_IN_PIXEL))
     # Method to draw the food on the board - 12
-    def draw(self):
-        pass
+    def draw(self, surface):
         # Create and draw a rectangle of a different color for the food - 41
+        food = pygame.Rect((self.position[0], self.position[1]), (CASE_SIZE_IN_PIXEL, CASE_SIZE_IN_PIXEL))
+        pygame.draw.rect(surface, self.color, food)
 
 
 # Function to draw the grid - 17
 def draw_grid(surface):
     # For each case of the grid - 18
-    for y in range(0, int(CASE_HEIGHT)):
-        for x in range(0, int(CASE_WIDTH)):
+    for y in range(0, int(NB_OF_CASE_HEIGHT)):
+        for x in range(0, int(NB_OF_CASE_WIDTH)):
             # If we are on an even case of the grid, we create and draw a rectangle of a first color - 19
             if (x+y) % 2 == 0:
-                r = pygame.Rect((x*CASE_SIZE, y*CASE_SIZE), (CASE_SIZE, CASE_SIZE))
+                r = pygame.Rect((x*CASE_SIZE_IN_PIXEL, y*CASE_SIZE_IN_PIXEL), (CASE_SIZE_IN_PIXEL, CASE_SIZE_IN_PIXEL))
                 pygame.draw.rect(surface, (93, 216, 228), r)
             # Otherwise, we do the same with another color - 20
             else:
-                rr = pygame.Rect((x*CASE_SIZE, y*CASE_SIZE), (CASE_SIZE, CASE_SIZE))
+                rr = pygame.Rect((x*CASE_SIZE_IN_PIXEL, y*CASE_SIZE_IN_PIXEL), (CASE_SIZE_IN_PIXEL, CASE_SIZE_IN_PIXEL))
                 pygame.draw.rect(surface, (84, 194, 205), rr)
 # Global variables (screen size, grid size, possible movements of the snake) - 13
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH_IN_PIXEL = 600
+SCREEN_HEIGHT_IN_PIXEL = 600
 
-CASE_SIZE = 20
-CASE_WIDTH = SCREEN_WIDTH / CASE_SIZE
-CASE_HEIGHT = SCREEN_HEIGHT / CASE_SIZE
+CASE_SIZE_IN_PIXEL = 20
+NB_OF_CASE_WIDTH = SCREEN_WIDTH_IN_PIXEL / CASE_SIZE_IN_PIXEL
+NB_OF_CASE_HEIGHT = SCREEN_HEIGHT_IN_PIXEL / CASE_SIZE_IN_PIXEL
 
 UP = (0, -1)
 DOWN = (0, 1)
@@ -108,13 +130,12 @@ RIGHT = (1, 0)
 
 
 def main():
-    pass
     # Initialize the game - 14
     pygame.init()
     pygame.display.set_caption("I'm a Snake !")
     # Set up (mettre en place) the clock and the screen - 15
     clock = pygame.time.Clock()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
+    screen = pygame.display.set_mode((SCREEN_WIDTH_IN_PIXEL, SCREEN_HEIGHT_IN_PIXEL), 0, 32)
     # Set up the game surface - 16
     surface = pygame.Surface(screen.get_size())
     surface = surface.convert()
@@ -124,28 +145,35 @@ def main():
     snake = Snake()
     food = Food()
     # Initiliaze the score - 23
-
+    snake.score = 0
     # Configure the font of the score - 47
-
+    pygame.font.init()
+    myfont = pygame.font.SysFont("monospace", 16)
     # The game loop - 24
     while (True):
         # Choose the number of frames per second - 25
-        clock.tick(10)
+        clock.tick(snake.speed)
         # Handle the pressed keys - 42
         snake.handle_keys()
         # Draw the grid - 43
-
+        draw_grid(surface)
         # Move the snake - 44
-
+        snake.move()
         # Check if the snake's head is on a food - 45
-
+        if snake.position[0] == food.position:
+            snake.length+=1
+            food.randomize_food()
+            snake.speed+=1
+            snake.score+=1
         # Draw the snake and the food - 46
         snake.draw(surface)
+        food.draw(surface)
         # Update and refresh the screen when an action occurs (a lieu) - 26
         screen.blit(surface, (0, 0))
-        pygame.display.update()
         # Display the score on the top left corner - 48
-
+        text = myfont.render("Score : {0}".format(snake.score), 1, (0, 0, 0))
+        screen.blit(text, (5, 10))
+        pygame.display.update()
 
 
 if __name__ == "__main__":
